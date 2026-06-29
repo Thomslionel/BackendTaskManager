f397d8d09f4ea3b1981ee2eb7f632b8c0d707cc2
Test <thomsmanly@gmail.com>
2026-06-29 22:46:31 +0000

    Mise a jour de mail Service

--- Middleware/CronJob.js
+++ Middleware/CronJob.js
@@ -3,7 +3,7 @@ const { Op } = require("sequelize");
 const { Task, User } = require("../Model/association");
 const { sendMail } = require("./mailService");
 
-cron.schedule("40 22 * * *", async () => {
+cron.schedule("50 22 * * *", async () => {
     console.log("🔔 Daily task reminder running...");
 
     try {
--- Middleware/mailService.js
+++ Middleware/mailService.js
@@ -25,16 +25,15 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, // false pour le port 587 (STARTTLS)
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, // Assurez-vous qu'il s'agit d'un "Mot de passe d'application" Gmail
+    pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 30000,      // 30s
+  connectionTimeout: 30000,
   greetingTimeout: 30000,
   socketTimeout: 30000,
-  // Optionnel : forcer l'IPv4 (parfois utile sur Render)
-  family: 4, // 10 secondes max pour se connecter
+  family: 4,   
 });
 
 function sendMail(to, subject, text) {
