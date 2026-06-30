d2795d9330a7c5f09a478b6aa636c00bb8cbc84b
Test <thomsmanly@gmail.com>
2026-06-30 06:35:19 +0000

    Mise a jour de mail Service

--- Middleware/CronJob.js
+++ Middleware/CronJob.js
@@ -3,7 +3,7 @@ const { Op } = require("sequelize");
 const { Task, User } = require("../Model/association");
 const { sendMail } = require("./mailService");
 
-cron.schedule("32 6 * * *", async () => {
+cron.schedule("37 6 * * *", async () => {
     console.log("🔔 Daily task reminder running...");
 
     try {
